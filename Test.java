adf780e664c5cd1093714e183d7b83ff6f46041f
Słuchacz <Suchacz>
2019-03-23 09:48:51 +0100

    "Remove commend":

---
+++ Test.java
@@ -0,0 +1,14 @@
+/*
+git cloneKlonujemy tylko raz na samym poczatku. Plik pobieramy ze strony gitHub.com
+git status - daje informacje
+git add nazwa_pliku.java - wtedy zmiany zmieniaja kolor na zielono, tzn.plik zostaje dodany do indexu
+git commit -m --> wtedy dodaje komentarz jako " "
+git push - otwiera sie okno do logowania / potem git status i musze wpisac swoj login i haslo - zreszta wszystko jest napisane, czytam w konciu instrukcje i operacja powinna zakonczyc sie na przeniesieniu do respozytorium zdalnego, czyli master
+
+*/
+
+public class Test {
+	public static void main(String[] args) {
+	
+	}
+}
