45741adfdd822b95b73eee9beb0b25e0c98bf49e
Słuchacz <Suchacz>
2019-03-23 10:03:45 +0100

    Added println

--- Test.java
+++ Test.java
@@ -1,14 +1,21 @@
 /*
 git cloneKlonujemy tylko raz na samym poczatku. Plik pobieramy ze strony gitHub.com
-git status - daje informacje
+git status - daje informacje o workspace
 git add nazwa_pliku.java - wtedy zmiany zmieniaja kolor na zielono, tzn.plik zostaje dodany do indexu
 git commit -m --> wtedy dodaje komentarz jako " "
-git push - otwiera sie okno do logowania / potem git status i musze wpisac swoj login i haslo - zreszta wszystko jest napisane, czytam w konciu instrukcje i operacja powinna zakonczyc sie na przeniesieniu do respozytorium zdalnego, czyli master
+git push - otwiera sie okno do logowania, dodaje do lokalnego repo / potem git status i musze wpisac swoj login i haslo - zreszta wszystko jest napisane, czytam w konciu instrukcje i operacja powinna zakonczyc sie na przeniesieniu do respozytorium zdalnego, czyli master
+no i znowu git status - pokazuje aktualny stan kodu
+git commit -a to to samo co git add " " - tak jak w podpowiedzi 
+git diff - pokazuje nam co sie zmienilo, zeby wyjsc z diffa trzeba macisnac q
+vim - wychodzenie :wq
+git branch - zmienia galezie
+
+
 
 */
 
 public class Test {
 	public static void main(String[] args) {
-	
+		System.out.println("test");
 	}
 }
